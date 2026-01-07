645eef56c64c90ba87313f86459abdd6c8cbca80
flamurjahiri <flamurjahiri@kodelabs.com>
2026-01-07 20:24:47 +0100

    init

--- src/main/java/io/kodelabs/ExampleResource.java
+++ src/main/java/io/kodelabs/ExampleResource.java
@@ -4,20 +4,20 @@ import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 public class ExampleResource {
 
     public static void main(String[] args) {
 
-        Function<AtomicInteger, Uni<? extends Integer>> fn = s -> {
-            return Uni.createFrom().item(s.get()).onItem().invoke(i -> System.out.println("Receiving on init: " + s.getAndIncrement()));
+        BiFunction<String, AtomicInteger, Uni<? extends Integer>> fn = (st, at) -> {
+            return Uni.createFrom().item(at.get()).onItem().invoke(i -> System.out.println(st + " Receiving on init: " + at.getAndIncrement()));
         };
 
 
         Multi.createBy().repeating().uni(
                         () -> new AtomicInteger(0),
-                        a -> Uni.createFrom().deferred(() -> fn.apply(a))
+                        a -> Uni.createFrom().deferred(() -> fn.apply("FIRST", a))
                 ).until(i -> true)
                 .collect()
                 .last() // first
@@ -27,23 +27,50 @@ public class ExampleResource {
                 }).subscribe().with(i -> System.out.println("Success"), f -> System.out.println(f.getMessage()));
 
 
-        System.out.println("First case finished");
+        try {
+            Thread.sleep(20);
+        } catch (Exception ign) {
+
+        }
+
         System.out.println();
         System.out.println();
+
 
         AtomicInteger count = new AtomicInteger(0);
 
         Multi.createBy().repeating().uni(
                         () -> new AtomicInteger(5),
-                        a -> Uni.createFrom().deferred(() -> fn.apply(a))
+                        a -> Uni.createFrom().deferred(() -> fn.apply("SECOND", a))
                 ).until(i -> count.getAndIncrement() == 1)
                 .collect()
                 .last() // first
                 .map(i -> {
-                    System.out.println("First case : Received after init: " + i);
+                    System.out.println("Second case : Received after init: " + i);
                     return i;
-                }).subscribe().with(i -> System.out.println("Success"), f -> System.out.println(f.getMessage()));
+                }).subscribe().with(i -> System.out.println("Second finished"), f -> System.out.println(f.getMessage()));
 
+
+        try {
+            Thread.sleep(20);
+        } catch (Exception ign) {
+
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        Multi.createBy().repeating().uni(
+                        () -> new AtomicInteger(17),
+                        a -> Uni.createFrom().deferred(() -> fn.apply("THIRD", a))
+                ).whilst(i -> false)
+                .collect()
+                .last() // first
+                .map(i -> {
+                    System.out.println("Third case : Received after init: " + i);
+                    return i;
+                }).subscribe().with(i -> System.out.println("Third finished"), f -> System.out.println(f.getMessage()));
 
     }
 }
